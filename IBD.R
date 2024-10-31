a0ac098a6d998112dc4e4ff6e220d9963232719e
Arthur Oulès <69710623+Arthur-Oules@users.noreply.github.com>
2024-10-31 10:33:23 +0100

    Split IBD to its own qmd, added send-away computations

--- IBD.R
+++
@@ -1,8 +0,0 @@
-library(here)
-library(dartR)
-
-lcwgs_gl <- readRDS(file = here("data", "lcwgs", "Embiotoca_lcwgs_gl.rds"))
-
-IBD <- gl.ibd(lcwgs_gl, coordinates = "latlon")
-
-IBD |> saveRDS(file = here("output", "Embiotocidae_IBD.rds"))
